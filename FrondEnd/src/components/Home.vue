801981d7554b2670ed11c08ab442b48e7f8e5902
Westongb <westongb@outlook.com>
2020-06-25 16:07:14 -0600

    Front End Folder

---
+++ FrondEnd/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <Scheduler/>
+    </div>
+</template>
+
+<script>
+
+import Scheduler from "./scheduler";
+
+export default {
+    name: "Home",
+    components: {
+        Scheduler
+    }
+   
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ FrondEnd/src/components/scheduler.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Schedulers</h1>
+    </div>
+</template>
+
+<script>
+export default {
+   name: "Scheduler"
+}
+</script>
+
+<style scoped>
+
+</style>
